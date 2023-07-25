f1efc120b7b2333e69689a61778d8e5258b3eb38
Mahmed <hello@manoons.com>
2023-07-25 13:35:26 +0500

    general comment also added directly in the file

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository which is forked.")
  print("hello there")
+ print("general file")
 git_opeation()
 #Just to add a comment here....
